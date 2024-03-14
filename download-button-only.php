e28443ba104065a6954446029a04e50b000ec250
Justin Henry <jhenry@uvm.edu>
2024-03-14 10:41:02 -0400

    Add support for standalone download buttons on video list page.

---
+++ download-button-only.php
@@ -0,0 +1,1 @@
+<a id="download" class="btn btn-outline-primary btn-sm" href="<?= $videoService->getUrl($video) . '?download=' . $video->videoId ?>" role="button"><i class="fas fa-file-download"></i> Download (<?= $filesize; ?>) </a>
